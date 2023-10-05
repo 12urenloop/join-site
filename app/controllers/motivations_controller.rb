1432d18e4afa1ddf12bddbe8633ed2eb5c5db64a
Tom Naessens <silox@zeus.ugent.be>
2023-10-05 20:22:03 +0200

    Make everything work

--- app/controllers/motivations_controller.rb
+++ app/controllers/motivations_controller.rb
@@ -1,5 +1,5 @@
 class MotivationsController < ApplicationController
-  http_basic_authenticate_with name: "admin", password:  ENV['APP_ADMIN_PASSWORD'], only: :index
+  http_basic_authenticate_with name: "admin", password: ENV['APP_ADMIN_PASSWORD'], only: :index
 
   def new
     @motivation = Motivation.new
